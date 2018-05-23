8ab7073b9c1a29b7ed9380ae3f16a30ed4e0e748
chenchangyan0222@126.com <chenchangyan0222@126.com>
2018-05-23 11:19:00 +0800

    放开各个交易所

--- app/schedule/s10.js
+++ app/schedule/s10.js
@@ -5,26 +5,5 @@ module.exports = {
     },
     async task(ctx) {
       ctx.app.weexHttps.setRate(ctx);
-    },
-    resetOtherInfo() {
-        // ctx.bian.reset24hr(ctx.helper.getMarkets()["USD"], function () {
-        //     console.log(ctx.ZB.get24hr());
-        //     console.log("init ZB end");
-        // });
-
-        // ctx.huobi.reset24hr(ctx.helper.getMarkets()["USD"], function () {
-        //     console.log(ctx.ZB.get24hr());
-        //     console.log("init huobi end");
-        // });
-
-        // ctx.okex.reset24hr(ctx.helper.getMarkets()["USD"], function () {
-        //     console.log(ctx.ZB.get24hr());
-        //     console.log("init okex end");
-        // });
-
-        // ctx.ZB.reset24hr(ctx.helper.getMarkets()["USD"], function () {
-        //     console.log(ctx.ZB.get24hr());
-        //     console.log("init ZB end");
-        // });
     }
 };
