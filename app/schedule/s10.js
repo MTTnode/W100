91f1e1660a3f8e6e8275266fa5ac4541683d1b5d
xtsong <764952224@qq.com>
2018-06-04 22:19:22 +0800

    极光配置

--- app/schedule/s10.js
+++ app/schedule/s10.js
@@ -1,7 +1,3 @@
-const moment = require("moment");
-const JPush = require("jpush-async").JPush;
-const client = JPush.buildClient('77bb191f81fb0b5cefffd2e0', '96401b931403dd89f4932627');
-
 module.exports = {
   schedule: {
     interval: '10s',
--- app/schedule/s10_tgmsg.js
+++ app/schedule/s10_tgmsg.js
@@ -1,3 +1,7 @@
+const moment = require("moment");
+const JPush = require("jpush-async").JPush;
+const client = JPush.buildClient('77bb191f81fb0b5cefffd2e0', '96401b931403dd89f4932627');
+
 module.exports = {
   schedule: {
     interval: '10s',
